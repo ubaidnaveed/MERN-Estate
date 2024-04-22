0097449ff3942cafb8c5f69339bfa5158a881c55
ubaidnaveed <ubaidub97@gmail.com>
2024-04-22 21:42:42 +0500

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,7 @@
 import User from "../models/user.model.js";
-//npm i bcryptjs
-import bcryptjs from "bcryptjs";
+import bcryptjs from "bcryptjs";  /*npm i bcryptjs*/
 import { errorHandler } from "../utils/error.js";
-
+import jwt from 'jsonwebtoken';  /*npm i jsonwebtoken*/
 
 export const signup = async(req, res, next) => {
     const {username, email, password} = req.body;
@@ -20,5 +19,22 @@ export const signup = async(req, res, next) => {
     }catch(error){
         next(error);
     }
-    }
+}
+
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    try{
+         const validUser = await User.findOne({email});
+         if (!validUser) return next(errorHandler(404,"User not found"));
+         const validPassword = await bcryptjs.compare(password, validUser.password);
+         if (!validPassword) return next(errorHandler(401,"Invalid Credentials"));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const{password:pass,...rest} = validUser._doc
+        res.cookie('access_token', token,{httponly: true}).status(200).json(rest)
+
+}catch(error){
+    next(error);
+}
+    
+}
    
