53280eeb04b268b9225cfae065fc8e1bb87b087a
ubaidnaveed <ubaidub97@gmail.com>
2024-05-02 22:03:13 +0500

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -265,10 +265,13 @@ export default function CreateListing() {
                 value={formData.regularPrice}
               />
               <div className='flex flex-col items-center'>
-                <p>Regular price</p>
+              <p>Regular price</p>
+              {formData.type === 'rent' && (
                   <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
+            {formData.offer && (
               <div className='flex items-center gap-2'>
                 <input
                   type='number'
@@ -279,16 +282,16 @@ export default function CreateListing() {
                   className='p-3 border border-gray-300 rounded-lg'
                   onChange={handleChange}
                   value={formData.discountPrice}
-
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
 
+                  {formData.type === 'rent' && (
                     <span className='text-xs'>($ / month)</span>
-
+                  )}
                 </div>
               </div>
-
+)}
           </div>
         </div>
         <div className='flex flex-col flex-1 gap-4'>
